9563bf8d70fe8fbc45b986db56e343b9b43b893f
Ondrej Brinkel <info@anzui.de>
2012-11-06 16:52:24 +0100

    added englisch translation

---
+++ languages/en/default.php
@@ -0,0 +1,6 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+$GLOBALS['TL_LANG']['el_references_found']  = 'This element is referenced %d time(s):';
+$GLOBALS['TL_LANG']['el_references_not_found']  = 'This element is not referenced.';
+$GLOBALS['TL_LANG']['tl_content']['el_references_legend']  = 'References';
+$GLOBALS['TL_LANG']['tl_article']['el_references_legend']  = $GLOBALS['TL_LANG']['tl_content']['el_references_legend'];
